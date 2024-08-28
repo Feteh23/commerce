3e1c034684d9e9344348cf3343857da166e0c3ea
Feteh23 <fetehmireillelareine@gmail.com>
2024-08-28 13:55:54 +0100

    first commit

--- lib/payment_pages.dart/payment_method.dart
+++ lib/payment_pages.dart/payment_method.dart
@@ -1,7 +1,5 @@
 import 'package:e_commerce/resources.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 
 class Payment_Method extends StatefulWidget {
